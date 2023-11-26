9c5aab1ddcfb616e53be2ec591c7963bb3443d52
Gabriel Souza <biel.als@icloud.com>
2023-11-26 14:36:30 -0300

    refactor: refatorando o código - deixando mais limpo

--- src/pages/homepage/home-page.style.ts
+++ src/pages/homepage/home-page.style.ts
@@ -1,9 +1,5 @@
 import { styled } from "@stitches/react";
 
-interface NavLinkProps {
-    isActive?: string,
-}
-
 export const HomePageContainer = styled('section', {
     backgroundColor: '#F0F0F5',
     width: '100%',
@@ -18,42 +14,6 @@ export const NavContainer = styled('section', {
     alignItems: 'center',
 })
 
-export const NavWrapper = styled('div', {
-    gap: '2.5rem',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'start',
-    listStyle: 'none',
-})
-
-export const Link = styled<NavLinkProps>('li',{
-    textTransform: 'uppercase',
-    fontWeight: '400',
-    fontSize: '18px',
-    variants: {
-        isActive: {
-            all: {
-                fontSize: '22px',
-                fontWeight: '600',
-                borderBottom: '4px solid var(--orange-low)'
-            },
-            't-shirts': {
-                fontSize: '22px',
-                fontWeight: '600',
-                borderBottom: '4px solid var(--orange-low)'
-            },
-            mugs: {
-                fontSize: '22px',
-                fontWeight: '600',
-                borderBottom: '4px solid var(--orange-low)'
-            }
-        }
-    },
-
-    '&:hover': {
-        cursor: 'pointer',
-    }
-})
 
 export const Organize = styled('div', {
     display: 'flex',
--- src/pages/homepage/home-page.tsx
+++ src/pages/homepage/home-page.tsx
@@ -1,40 +1,19 @@
 import { Header, saira } from "@/components/header/header";
-import { HomePageContainer, Link, NavContainer, NavWrapper, Organize } from "./home-page.style";
-import { useStore } from "@/hooks/useStore";
+import { HomePageContainer, NavContainer, Organize } from "./home-page.style";
 import { ArrowIcon } from "@/components/arrow-icon";
-
-const NavLinks = [
-    {title: "Todos os Produtos", category: "all"},
-    {title: "Camisetas", category: "t-shirts"},
-    {title: "Canecas", category: "mugs"},
-]
+import { FilterByType } from "@/components/filter-by-type/filter-by-type";
 
 export function HomePage(){
-    const { typeLink, handleSetTypeLink } = useStore()
     return(
         <>
             <Header />
             <HomePageContainer className={saira.className}>
                 <NavContainer>
-                    <NavWrapper>
-                        {
-                            NavLinks.map((option) => {
-                                return (
-                                    <Link
-                                        key={option.category}
-                                        isActive={option.category === typeLink ? typeLink : ''}
-                                        onClick={() => handleSetTypeLink(option.category)}
-                                    >
-                                        {option.title}
-                                    </Link>
-                                )
-                            })
-                        }
-                    </NavWrapper>
-                    <Organize>
-                        <p>Organizar por </p>
-                        <ArrowIcon />
-                    </Organize>
+                <FilterByType />
+                <Organize>
+                    <p>Organizar por </p>
+                    <ArrowIcon />
+                </Organize>
                 </NavContainer>
             </HomePageContainer>
         </>
